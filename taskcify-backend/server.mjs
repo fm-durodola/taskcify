7f1349705b87fd6b4e6d15cbd0445548d1522705
John O.O Durodola <fm.durodola@gmail.com>
2024-06-06 07:41:28 -0700

    add all neccessary package and startup code in server.mjs and enviroment variable to constant.mjs

--- taskcify-backend/server.mjs
+++ taskcify-backend/server.mjs
@@ -1,1 +1,44 @@
+import "dotenv/config";
 import express from "express";
+import session from "express-session";
+import cors from "cors";
+import MongoStore from "connect-mongo";
+
+import {
+  MONGO_URI,
+  ORIGIN_ALLOW,
+  PORT,
+  SESSION_SECRET,
+} from "./utils/constants.mjs";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ORIGIN_ALLOW,
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    store: MongoStore.create({ mongoUrl: MONGO_URI }),
+  })
+);
+
+// Notfound Error Middleware
+app.use((req, res, next) => {
+  res.status(404).send({ errorMessage: "Page Not Found" });
+
+  next();
+});
+
+// General Error Middleware
+app.use((error, req, res, next) => {
+  res.status(500).send({ errorMessage: "Server error" });
+});
+
+app.listen(PORT, console.log(`Server running on PORT : ${PORT}`));
---
+++ taskcify-backend/utils/constants.mjs
@@ -0,0 +1,6 @@
+
+
+export const MONGO_URI = process.env.MONGO_URI;
+export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const PORT = process.env.PORT;
+export const ORIGIN_ALLOW = process.env.ORIGIN_ALLOW;
